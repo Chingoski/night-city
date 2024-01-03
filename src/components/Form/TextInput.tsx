2607777069493667c2843e29a8c17b30af0b1d08
Nikola <chingo@upshiftwork.com>
2024-01-04 00:59:41 +0100

    feat: Fixed typescript issues in Form components

--- src/components/Form/TextInput.tsx
+++ src/components/Form/TextInput.tsx
@@ -1,32 +1,36 @@
 import { Input, FormControl, FormErrorMessage } from "@chakra-ui/react";
 import { Field, FieldInputProps, FormikProps } from "formik";
 
-function TextInput(props: {name: string, placeholder: string}) {
-    let name = props.name;
-    let placeholder = props.placeholder;
+function TextInput(props: { name: string; placeholder: string }) {
+  let name = props.name;
+  let placeholder = props.placeholder;
 
-    return (
-        <Field name={name}>
-            {({
-                field,
-                form,
-            }: {
-                field: FieldInputProps<string>;
-                form: FormikProps<any>; // Changed to 'any' to handle dynamic field names
-            }) => (
-                <FormControl
-                    isInvalid={
-                        form.errors[name] && form.touched[name]
-                    }
-                >
-                    <Input {...field} placeholder={placeholder} /> 
-                    {/* Use name for placeholder, or customize as needed */}
-                    <FormErrorMessage>{form.errors[name]}</FormErrorMessage>
-                </FormControl>
-            )}
-        </Field>
-    );
+  return (
+    <Field name={name}>
+      {({
+        field,
+        form,
+      }: {
+        field: FieldInputProps<string>;
+        form: FormikProps<any>;
+      }) => (
+        <FormControl
+          isInvalid={
+            form.errors[name] != "" &&
+            form.errors[name] != undefined &&
+            form.touched[name] == true
+          }
+        >
+          <Input {...field} placeholder={placeholder} />
+          <FormErrorMessage>
+            {typeof form.errors[name] === "string"
+              ? (form.errors[name] as string)
+              : "Invalid input"}
+          </FormErrorMessage>
+        </FormControl>
+      )}
+    </Field>
+  );
 }
-
 
 export default TextInput;
