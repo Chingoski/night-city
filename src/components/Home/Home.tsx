bbeb65dec2a76392b09a230399486e22979e00f7
Chingoski <chingoski.nikola@gmail.com>
2024-01-04 18:43:50 +0100

    Added fetching function for the All Listings section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,46 @@
 import { Flex } from "@chakra-ui/react";
+
+import { allListingsContext } from "../../context/AllListingsContext";
+import { useContext, useEffect } from "react";
+
+import axios from "axios";
+import host from "../../host";
+import { getAuthToken } from "../../util/auth";
+
 import TopMenu from "../TopMenu/TopMenu";
 import ListingCard from "../ListingCard/ListingCard";
 
 function Home() {
+  const { allListings, setAllListings } = useContext(allListingsContext);
+
+  const fetchListings = async () => {
+    const token = getAuthToken();
+    const response = await axios.get(`${host}/api/game_listings`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": "true",
+      },
+    });
+
+    if (response.status !== 200) {
+      setAllListings([]);
+    }
+
+    setAllListings(response.data.data);
+  };
+
+  useEffect(() => {
+    fetchListings();
+  }, []);
+
   return (
     <Flex flexDirection="column" w="100%">
       <TopMenu />
       <Flex>
-        <ListingCard />
+        {/* {allListings.map((listing) => (
+          <ListingCard key={listing.id} listing={listing} />
+        ))} */}
       </Flex>
     </Flex>
   );
