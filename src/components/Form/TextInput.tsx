6061af2f771c428aaec086f86fc86ac1b5fc3b52
Chingoski <chingoski.nikola@gmail.com>
2024-01-19 14:33:41 +0100

    feat: Created contact us component and added business logic for it

--- src/components/Form/TextInput.tsx
+++ src/components/Form/TextInput.tsx
@@ -1,11 +1,13 @@
-import { Input, FormControl, FormErrorMessage } from "@chakra-ui/react";
+import { Input, FormControl, FormErrorMessage, background } from "@chakra-ui/react";
 import { Field, FieldInputProps, FormikProps } from "formik";
 
-function TextInput(props: { name: string; placeholder: string, type: string }) {
+function TextInput(props: { name: string; placeholder: string; type: string, background?: string }) {
   const name = props.name;
   const placeholder = props.placeholder;
   const type = props.type;
-
+  
+  const backgroundStyle = background ? {background: props.background} : {};
+ 
   return (
     <Field name={name}>
       {({
@@ -22,7 +24,7 @@ function TextInput(props: { name: string; placeholder: string, type: string }) {
             form.touched[name] == true
           }
         >
-          <Input {...field} placeholder={placeholder} type={type}/>
+          <Input w="100%" {...field} placeholder={placeholder} type={type} {...backgroundStyle}/>
           <FormErrorMessage>
             {typeof form.errors[name] === "string"
               ? (form.errors[name] as string)
--- src/pages/ContactUsPage.tsx
+++ src/pages/ContactUsPage.tsx
@@ -1,10 +1,16 @@
-import { Flex, useStyleConfig, Text } from "@chakra-ui/react";
+import {
+  useStyleConfig,
+  Flex,
+
+} from "@chakra-ui/react";
+
+
 
 import { navigationContext } from "../context/NavigationContext";
 import { useContext } from "react";
+import ContactUs from "../components/ContactUs/ContactUs";
 
 function ContactUsPage() {
-
   const { isCollapsed } = useContext(navigationContext);
   const styles = useStyleConfig("Home");
 
@@ -12,6 +18,9 @@ function ContactUsPage() {
     <Flex
       flexDirection="column"
       marginBottom="15px"
+      justifyContent="center"
+      alignItems="center"
+      width="100%"
       sx={{
         ...styles,
         width: isCollapsed
@@ -22,7 +31,7 @@ function ContactUsPage() {
           : "var(--open-nav-width)",
       }}
     >
-      <Text> Test</Text>
+     <ContactUs/>
     </Flex>
   );
 }
