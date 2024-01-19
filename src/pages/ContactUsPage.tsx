bee21e6e056f23b19322c9dbab60e061d6dd12c6
Chingoski <chingoski.nikola@gmail.com>
2024-01-19 12:16:30 +0100

    chore: Removed HelpUs page and navigation and refactored site name

--- src/pages/ContactUsPage.tsx
+++ src/pages/ContactUsPage.tsx
@@ -1,5 +1,30 @@
+import { Flex, useStyleConfig, Text } from "@chakra-ui/react";
+
+import { navigationContext } from "../context/NavigationContext";
+import { useContext } from "react";
+
 function ContactUsPage() {
-  return <div>ContactUsPage</div>;
+
+  const { isCollapsed } = useContext(navigationContext);
+  const styles = useStyleConfig("Home");
+
+  return (
+    <Flex
+      flexDirection="column"
+      marginBottom="15px"
+      sx={{
+        ...styles,
+        width: isCollapsed
+          ? "var(--collapsed-outlet-width)"
+          : "var(--open-outlet-width)",
+        marginLeft: isCollapsed
+          ? "var(--collapsed-nav-width)"
+          : "var(--open-nav-width)",
+      }}
+    >
+      <Text> Test</Text>
+    </Flex>
+  );
 }
 
 export default ContactUsPage;
