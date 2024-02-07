7f2f78070bf95179cda0d5471787d65ebb349462
Chingoski <chingoski.nikola@gmail.com>
2024-02-07 20:51:19 +0100

    feat: added badge to show confirmation status by trader and owner

--- src/components/TradeOffers/TradeOffersActions/ConfirmTradeButton.tsx
+++ src/components/TradeOffers/TradeOffersActions/ConfirmTradeButton.tsx
@@ -8,7 +8,8 @@ import { tradeType } from "../../../types/trade-type";
 const ConfirmTradeButton: React.FC<{
   onOpen: () => void;
   trade: tradeType;
-}> = ({ onOpen, trade }) => {
+  type: string;
+}> = ({ onOpen, trade, type }) => {
   const { setToConfirmTrade } = useContext(tradesActionsContext);
 
   function confirmTradeHandler() {
@@ -16,17 +17,60 @@ const ConfirmTradeButton: React.FC<{
     setToConfirmTrade(trade);
   }
   return (
-    <Button
-      borderColor="teal.400"
-      variant="outline"
-      _hover={{ backgroundColor: "teal.50" }}
-      onClick={confirmTradeHandler}
-    >
-      <Icon as={FaHandshake} color="teal.400" />
-      <Text ml="5px" color="teal.400">
-        Confirm
-      </Text>
-    </Button>
+    <>
+      {type === "received" &&
+        (trade.owner_confirmed ? (
+          <Button
+            borderColor="gray.400"
+            variant="outline"
+            _hover={{ backgroundColor: "gray.50", cursor: "auto" }}
+            disabled={true}
+          >
+            <Icon as={FaHandshake} color="gray.400" />
+            <Text ml="5px" color="gray.400">
+              Confirmed
+            </Text>
+          </Button>
+        ) : (
+          <Button
+            borderColor="teal.400"
+            variant="outline"
+            _hover={{ backgroundColor: "teal.50" }}
+            onClick={confirmTradeHandler}
+          >
+            <Icon as={FaHandshake} color="teal.400" />
+            <Text ml="5px" color="teal.400">
+              Confirm
+            </Text>
+          </Button>
+        ))}
+      {type === "sent" &&
+        (trade.trader_confirmed ? (
+          <Button
+            borderColor="gray.400"
+            variant="outline"
+            _hover={{ backgroundColor: "gray.50", cursor: "auto" }}
+            disabled={true}
+          >
+            <Icon as={FaHandshake} color="gray.400" />
+            <Text ml="5px" color="gray.400">
+              Confirmed
+            </Text>
+          </Button>
+        ) : (
+          <Button
+            borderColor="teal.400"
+            variant="outline"
+            _hover={{ backgroundColor: "teal.50" }}
+            onClick={confirmTradeHandler}
+          >
+            <Icon as={FaHandshake} color="teal.400" />
+            <Text ml="5px" color="teal.400">
+              Confirm
+            </Text>
+          </Button>
+        ))}
+    </>
   );
 };
 
--- src/host.ts
+++ src/host.ts
@@ -1,3 +1,3 @@
-const host: string = "https://14ac-77-29-21-134.ngrok-free.app";
+const host: string = "https://499a-77-29-35-90.ngrok-free.app";
 
 export default host;
